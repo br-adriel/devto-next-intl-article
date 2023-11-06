ebcefae1818bd6c6575876d5d46dcd8ae52a5bee
Adriel <adriel.fsantos@outlook.com>
2023-11-06 20:30:06 -0300

    Refactor page to use created components

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,37 +1,15 @@
-'use client';
-
-import { useTranslations } from 'next-intl';
+import DeployCard from '@/components/DeployCard';
+import Description from '@/components/Description';
+import DocsCard from '@/components/DocsCard';
+import LearnCard from '@/components/LearnCard';
+import TemplatesCard from '@/components/TemplatesCard';
 import Image from 'next/image';
 import styles from '../page.module.css';
 
 export default function Home() {
-  const t = useTranslations('home.page');
-
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          {t('get-started')}&nbsp;
-          <code className={styles.code}>src/app/[locale]/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            {t('by') + ' '}
-            <Image
-              src='/vercel.svg'
-              alt='Vercel Logo'
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <Description />
 
       <div className={styles.center}>
         <Image
@@ -45,54 +23,10 @@ export default function Home() {
       </div>
 
       <div className={styles.grid}>
-        <a
-          href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className={styles.card}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2>
-            {t('docs.title')} <span>-&gt;</span>
-          </h2>
-          <p>{t('docs.content')}</p>
-        </a>
-
-        <a
-          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className={styles.card}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2>
-            {t('learn.title')}
-            <span>-&gt;</span>
-          </h2>
-          <p>{t('learn.content')}</p>
-        </a>
-
-        <a
-          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className={styles.card}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2>
-            {t('templates.title')} <span>-&gt;</span>
-          </h2>
-          <p>{t('templates.content')}</p>
-        </a>
-
-        <a
-          href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className={styles.card}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2>
-            {t('deploy.title')} <span>-&gt;</span>
-          </h2>
-          <p>{t('deploy.content')}</p>
-        </a>
+        <DocsCard />
+        <LearnCard />
+        <TemplatesCard />
+        <DeployCard />
       </div>
     </main>
   );
